c7f59a6048acaa6ce2dbb78e753d4d3f05716a7b
Sebastian Golijow <cevichesmac@Sebastians-MBP.home>
2024-03-24 19:35:11 +0100

    add all views and serializers

--- authots_api/urls.py
+++ authots_api/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from dj_rest_auth.views import PasswordResetConfirmView
+from core_apps.users.views import CustomUserDetailsView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -19,6 +21,15 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0)),
     path(settings.ADMIN_URL, admin.site.urls),
+    path("api/v1.auth.user", CustomUserDetailsView.as_view(), name="user-details"),
+    path("api/v1/auth", include("dj_rest_auth.urls")),
+    path("api/v1/auth/registration", include("dj_rest_auth.registration.urls")),
+    path(
+        "api/v1/auth/password/rest/confirm/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path("api/v1/profiles/", include("core_apps.profiles.urls")),
 ]
 
 admin.site.site_header = "No Shortcuts"
--- core_apps/profiles/views.py
+++ core_apps/profiles/views.py
@@ -1,3 +1,133 @@
-from django.shortcuts import render
+from authots_api.settings.local import DEFAULT_FROM_EMAIL
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, status
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser
 
-# Create your views here.
+from .exeptions import CantFollowYourself
+from .models import Profile
+from .pagination import ProfilePagination
+from .renderers import ProfileJSONRenderer, ProfilesJSONRenderer
+from .serializers import ProfileSerializer, FollowingSerializer, UpdateProfileSerializer
+
+User = get_user_model()
+
+
+class ProfileListAPIView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    pagination_class = ProfilePagination
+    renderer_classes = [ProfilesJSONRenderer]
+
+
+class ProfileDetailAPIView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ProfileSerializer
+    renderer_classes = [ProfileJSONRenderer]
+
+    def get_queryset(self):
+        return Profile.objects.select_related("user")
+
+    def get_object(self):
+        user = self.request.user
+        return self.get_queryset().get(user=user)
+
+
+class UpdatePRofileAPIView(generics.RetrieveAPIView):
+    serializer_class = UpdateProfileSerializer
+    permission_classes = [IsAuthenticated]
+    renderer_classes = [ProfileJSONRenderer]
+    parser_classes = [MultiPartParser]
+
+    def get_object(self):
+        return self.request.user.profile
+
+    def patch(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class FollowerListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        try:
+            profile = Profile.objects.get(user__id=request.user.id)
+            follower_profile = profile.followers.all()
+            serializer = FollowingSerializer(follower_profile, many=True)
+            formatted_response = {
+                "status_code": status.HTTP_200_OK,
+                "follower_count": follower_profile.count(),
+                "followers": serializer.data,
+            }
+            return Response(formatted_response, status=status.HTTP_200_OK)
+        except Profile.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class FollowingListView(APIView):
+    def get(self, request, user_id, format=None):
+        try:
+            profile = Profile.objects.get(user__id=user_id)
+            following_profile = profile.following.all()
+            users = [p.user for p in following_profile]
+            serializer = FollowingSerializer(users, many=True)
+            formatted_response = {
+                "status_code": status.HTTP_200_OK,
+                "follower_count": following_profile.count(),
+                "user_I_follow": serializer.data,
+            }
+            return Response(formatted_response, status=status.HTTP_200_OK)
+        except Profile.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class FollowAPIView(APIView):
+    def post(self, request, user_id, format=None):
+        try:
+            follower = Profile.objects.get(user=self.request.user)
+            user_profile = request.user.profile
+            profile = Profile.objects.get(user__id=user_id)
+
+            if profile == follower:
+                raise CantFollowYourself()
+
+            if user_profile.check_following(profile):
+                formatted_response = {
+                    "status_code": status.HTTP_400_BAD_REQUEST,
+                    "message": "You are already following this user",
+                }
+                return Response(formatted_response, status=status.HTTP_400_BAD_REQUEST)
+
+            user_profile.follow(profile)
+            subject = "A new user follows you"
+            message = "TEST"
+            from_email = DEFAULT_FROM_EMAIL
+            recipient_list = [profile.user.email]
+            send_mail(subject, message, from_email, recipient_list, fail_silently=True)
+            return Response({"status_code": status.HTTP_200_OK, "message": "test"})
+        except Profile.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class UnfollowAPIView(APIView):
+    def post(self, request, user_id, format=None):
+        user_profile = request.user.profile
+        profile = Profile.objects.get(user_id=user_id)
+
+        if not user_profile.check_following(profile):
+            formatted_response = {
+                "status_code": status.HTTP_400_BAD_REQUEST,
+                "message": "TEST",
+            }
+            return Response(formatted_response, status.HTTP_400_BAD_REQUEST)
+
+        user_profile.unfollow(profile)
+        return Response({"status_code": status.HTTP_200_OK, "message": "test"})
